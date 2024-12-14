9c13a5c15824253f70100e6bb285c004cba0defe
Akshay Subramanian <akshay_s@mit.edu>
2024-12-14 18:54:38 -0500

    Copy paste frag decomp code and add scale param to some functions - WIP

--- diffcsp/common/data_utils.py
+++ diffcsp/common/data_utils.py
@@ -1997,41 +1997,7 @@ def get_fragment_atom_mapping_with_smarts(fragments, mol):
     return fragment_atom_mapping, cg_beads
 
 if __name__ == '__main__':
-    # smiles = "CCCCC(CC)Cn1c2c3sc(C=C4C(=O)c5cc(F)c(F)cc5C4=C(C#N)C#N)c(CCCCCCCCCCC)c3sc2c2c3nsnc3c3c4sc5c(CCCCCCCCCCC)c(C=C6C(=O)c7cc(F)c(F)cc7C6=C(C#N)C#N)sc5c4n(CC(CC)CCCC)c3c21"
-    # frag_obj = FragmentDecomp(smiles)
-    # fragments = frag_obj.get_fragments()  # This is assumed to provide SMARTS strings for fragments
-    # smiles = ".".join([smiles] * 5)
-    # mol = make_rdkit_mol(None, None, '/home/gridsan/sakshay/experiments/flowmm/y6_5_frames/train/frame0.pdb', smiles)
-    
-    # # Create a dictionary mapping fragments (SMARTS) to their respective atom indices in mol
-    # fragment_atom_mapping = get_fragment_atom_mapping_with_smarts(fragments, mol)
-
-    # # Print the results
-    # for frag, indices in fragment_atom_mapping.items():
-    #     print(f"Fragment (SMARTS): {frag}")
-    #     print(f"Atom Indices: {indices}")
-    #     print(len(indices))
-
-    # Toy Example (1D Fractional Coordinates)
-    frac_coords = torch.tensor([
-        [0.1, 0.1, 0.1],
-        [0.85, 0.85, 0.85],
-        [0.2, 0.2, 0.2],
-        [0.8, 0.8, 0.8],
-        [0.15, 0.15, 0.15],
-        [0.85, 0.85, 0.85]
-    ])
-    # frac_coords = torch.tensor([torch.tensor([0.1, 0.1, 0.1]), torch.tensor([0.9, 0.9, 0.9]), torch.tensor([0.2, 0.2, 0.2]), torch.tensor([0.8, 0.8, 0.8]), torch.tensor([0.15, 0.15, 0.15]), torch.tensor([0.85, 0.85, 0.85])])  # Fractional coordinates of atoms
-
-    # Bead-to-Atom Mapping (1D example)
-    cg_beads = [torch.tensor([0, 1]), torch.tensor([2, 3]), torch.tensor([4, 5])]
-
-    # Print the input data
-    print("Fractional Coordinates (All Atoms):", frac_coords)
-    print("Bead-to-Atom Mapping:", cg_beads)
-
-    # Calculate bead fractional coordinates using Fréchet mean
-    bead_frac_coords = calculate_bead_frac_coords(frac_coords, cg_beads, dist)
-
-    # Print the results
-    print("\nBead Fractional Coordinates (Intrinsic Mean):", bead_frac_coords)
+    smiles = "CCCCC(CC)Cn1c2c3sc(C=C4C(=O)c5cc(F)c(F)cc5C4=C(C#N)C#N)c(CCCCCCCCCCC)c3sc2c2c3nsnc3c3c4sc5c(CCCCCCCCCCC)c(C=C6C(=O)c7cc(F)c(F)cc7C6=C(C#N)C#N)sc5c4n(CC(CC)CCCC)c3c21"
+    frag_obj = FragmentDecomp(smiles)
+    fragments = frag_obj.get_fragments()
+    print(fragments)
